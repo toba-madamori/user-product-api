87351aa7a18eee5bd7943c673e55abe06c44e9b9
Toba <tobajamezmadamori@gmail.com>
2022-12-11 17:09:56 +0100

    added create-user-handler

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,5 +1,16 @@
 import { Request, Response } from 'express'
+import logger from '../utils/logger'
+import { createUser } from '../service/user.service'
+import { CreateUserInput } from '../schema/user.schema'
+import { omit } from 'lodash'
 
-export const createUser = (req: Request, res: Response) => {
-    
+export const createUserHandler = async (req: Request<{}, {}, CreateUserInput['body']>, res: Response) => {
+    try {
+        const newUser = await createUser(req.body)
+        const user = omit(newUser.toJSON(), ['password'])
+        return res.status(201).json({ user })
+    } catch (error: any) {
+        logger.error(error)
+        return res.status(409).send(error.message)
+    }
 }
