1f9e0c78f15ed83a1a9fd9e69062269ffc282d80
Toba <tobajamezmadamori@gmail.com>
2022-12-20 17:13:04 +0100

    added get sessions service

--- src/service/session.service.ts
+++ src/service/session.service.ts
@@ -1,6 +1,11 @@
-import SessionModel from '../models/session.model'
+import { FilterQuery } from 'mongoose'
+import SessionModel, { SessionDocument } from '../models/session.model'
 
 export const createSession = async (userid:string, userAgent: string) => {
     const session = await SessionModel.create({ user: userid, userAgent })
     return session.toJSON()
 }
+
+export const getSessions =async (query: FilterQuery<SessionDocument>) => {
+    return SessionModel.find(query).lean()
+} 
