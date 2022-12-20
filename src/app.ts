fc96056fe9e3c6e0c86fbda1f8632358d94b6b9d
Toba <tobajamezmadamori@gmail.com>
2022-12-20 17:14:46 +0100

    added deserializer middleware

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import config from 'config'
 import connect from './utils/connect'
 import logger from './utils/logger'
 import routes from './routes'
+import deserializeUser from './middleware/deserializeUser'
 
 const port = config.get<number>('port')
 
 const app = express()
 
 app.use(express.json())
+app.use(deserializeUser)
 
 const start = async () => {
     try {
