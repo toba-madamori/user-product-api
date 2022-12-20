05dab3fbb5c72b67e8ae97f134c2895fb8484d3e
Toba <tobajamezmadamori@gmail.com>
2022-12-20 16:24:39 +0100

    added create session endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Express, Request, Response } from 'express'
+import { createUserSessionHandler } from './controllers/session.controllers'
 import { createUserHandler } from './controllers/user.controllers'
 import validateResource from './middleware/validateResource'
+import { createSessionSchema } from './schema/session.schema'
 import { createUserSchema } from './schema/user.schema'
 
 const routes = (app: Express) => {
@@ -9,6 +11,8 @@ const routes = (app: Express) => {
     })
 
     app.post('/api/users', validateResource(createUserSchema), createUserHandler)
+
+    app.post('/api/sessions', validateResource(createSessionSchema), createUserSessionHandler)
 }
 
 export default routes
